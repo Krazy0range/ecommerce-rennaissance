c35ddd78d30be0060d0c687828c15a6380c32592
Krazy0range <77130315+Krazy0range@users.noreply.github.com>
2024-09-28 15:08:32 -0500

    refactor, remove

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from crawler import Crawler
 
-if __name__ == "__main__":
 
-    crawler = Crawler(
-        "www.walmart.com",
-        "database/urls.csv",
+def init_crawler(url, store):
+    return Crawler(
+        url,
+        f"database/{store}_urls.csv",
         0,
         debug=False,
     )
-    crawler.crawl()
+
+
+if __name__ == "__main__":
+
+    # walmart_crawler = init_crawler("www.walmart.com", "walmart")
+    # walmart_crawler.crawl()
+
+    costco_crawler = init_crawler("www.costco.com", "costco")
+    costco_crawler.crawl()
